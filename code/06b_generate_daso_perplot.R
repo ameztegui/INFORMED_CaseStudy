469ec68f3f4aa5937456a1654192a7ec28022722
Aitor Ameztegui <ameztegui@gmail.com>
2020-07-02 19:41:51 +0200

    include relative oaths using "here"

--- code/06b_generate_daso_perplot.R
+++ code/06b_generate_daso_perplot.R
@@ -1,3 +1,6 @@
+library(tidyverse)
+library(trelliscopejs)
+
 load("./data/data_plot.Rdata")
 load("./data/data_plot_species.Rdata")
 
@@ -7,19 +10,19 @@ data_plot %>%
     ggplot(aes(x=Timestep, y=BA)) +
     geom_line(aes(color = RCP)) +
     facet_trelliscope(Parcela ~ Management , nrow = 2, ncol = 4, width = 300, 
-                        path = "C:/Dades/Activity/INFORMED/reports/BA_per_plot")
+                        path = here("reports/BA_per_plot"))
 
 data_plot %>%
     ggplot(aes(x=Timestep, y=N)) +
     geom_line(aes(color = RCP)) +
     facet_trelliscope(Parcela ~ Management , nrow = 2, ncol = 4, width = 300, 
-                      path = "C:/Dades/Activity/INFORMED/reports/N_per_plot")
+                      path = here("/reports/N_per_plot"))
 
 data_plot %>%
     ggplot(aes(x=Timestep, y=DBH)) +
     geom_line(aes(color = RCP)) +
     facet_trelliscope(Parcela ~ Management , nrow = 2, ncol = 4, width = 300, 
-                      path = "C:/Dades/Activity/INFORMED/reports/DBH_per_plot/")
+                      path = here("reports/DBH_per_plot/"))
 
 
 
@@ -29,16 +32,16 @@ data_plot_species %>%
     ggplot(aes(x=Timestep, y=BA)) +
     geom_line(aes(color = Species)) +
     facet_trelliscope(Parcela ~ Management + RCP , nrow = 2, ncol = 4, width = 300, 
-                      path = "C:/Dades/Activity/INFORMED/reports/BA_per_plotspecies")
+                      path = here("reports/BA_per_plotspecies"))
 
 data_plot_species %>%
     ggplot(aes(x=Timestep, y=N)) +
     geom_line(aes(color = Species)) +
     facet_trelliscope(Parcela ~ Management + RCP , nrow = 2, ncol = 4, width = 300, 
-                      path = "C:/Dades/Activity/INFORMED/reports/N_per_plotspecies")
+                      path = here("reports/N_per_plotspecies"))
 
 data_plot_species %>%
     ggplot(aes(x=Timestep, y=DBH)) +
     geom_line(aes(color = Species)) +
     facet_trelliscope(Parcela ~ Management + RCP , nrow = 2, ncol = 4, width = 300, 
-                      path = "C:/Dades/Activity/INFORMED/reports/DBH_per_plotspecies/")
+                      path = here("reports/DBH_per_plotspecies/"))
